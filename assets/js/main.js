f66d4e7f5b08a053a290dd852e161b07fe386f1c
Michael <mammoliti.michael@gmail.com>
2016-02-24 22:31:09 +0100

    New version
    - Better sass
    - Logo
    - Syntax highlighter included (my theme)

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+String.prototype.escape = function() {
+    var tagsToReplace = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;'
+    };
+    return this.replace(/[&<>]/g, function(tag) {
+        return tagsToReplace[tag] || tag;
+    });
+};
+
+
+
+(function(){
+  var $spans = $(".block").find("span");
+
+  $.each($spans, function()
+  {
+    var $this       = $(this),
+        $spanParent = $this.closest(".col"),
+        className   = $spanParent.attr("class");
+
+    $(this).text(className);
+  });
+
+}());
+
+(function(){
+
+  $("pre code").not(".lang-css").each(function()
+  {
+    var $pre = $(this),
+        $text = $pre.html();
+
+    $pre.html($text.escape());
+  })
+
+}());
